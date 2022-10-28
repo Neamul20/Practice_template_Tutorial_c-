0d023ce20eb54bfb89c48ba84d7090cbaa5eb22b
Neamul20 <neamul3239@gmail.com>
2022-10-28 19:08:59 +0200

    Added Linked class

--- src/practice_Template_Class and function.cpp
+++ src/practice_Template_Class and function.cpp
@@ -8,7 +8,60 @@
 
 #include <iostream>
 using namespace std;
-template <class T>
+class Node{
+public:
+	int data;
+	Node* next;
+
+	Node(){
+			data=0;
+			next=NULL;
+	}
+	Node(int data){
+		this->data=data;
+		this-> next=NULL;
+	}
+};
+
+class Linkedlist {
+	Node* head; Node* tail;
+public:
+	Linkedlist(){
+		head=NULL;
+		tail=NULL;
+	}
+
+	void push_back(int data){
+		Node* newNode= new Node(data);
+		if (head==NULL){head=newNode; tail=newNode;}
+	else {
+		tail->next=newNode;
+		tail=newNode;
+		}
+	}
+	int pop_front(){
+		if (head==NULL) return -1;
+		Node* temp=head;
+		int poped_data= head->data;
+		head=head->next;
+		delete temp;
+
+		return poped_data;
+
+
+	}
+
+	void print_list(){
+		Node*temp=head;
+		while(temp!=NULL){
+			cout<<temp->data<<",";
+			temp=temp->next;
+		}
+		cout<<endl;
+	}
+
+};
+/*template <class T>
 void _swap(T &a, T &b){  //Template Function
 	T temp=b;
 	b=a;
@@ -17,13 +70,19 @@ void _swap(T &a, T &b){  //Template Function
 void print_ (int a ){
 cout<<"The output is "<<a<<endl;
 }
-
+*/
 int main() {
-	int a=10,b=20;
-	cout<<"a= "<<a<<"\n"<<"b= "<<b<<endl;
-	_swap (a,b);
-	cout<<"a= "<<a<<"\n"<<"b= "<<b<<endl;
-	print_ (2);
+	Linkedlist ll;
+	ll.push_back(0);
+	ll.push_back(1);
+	ll.push_back(2);
+	ll.push_back(3);
+	ll.push_back(4);
 
+	ll.print_list();
+
+	cout<<ll.pop_front()<<endl;;
+
+	ll.print_list();
 	return 0;
 }
